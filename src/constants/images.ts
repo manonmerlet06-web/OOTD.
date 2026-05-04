f573219a616e2fdec7c622528af3bcae9af5835b
manonmerlet06-web <manonmerlet06@gmail.com>
2026-05-04 12:41:32 +0200

    refactor: Update image URLs to use Googleusercontent
    Replaced Google Drive direct links with Googleusercontent URLs for improved image loading performance and reliability within the OOTD application.

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -3,10 +3,10 @@
  * Format : https://drive.google.com/drive/folders/1Npo3W2tU_AKAaM7f1ijNIwbKi1Nadbj7?usp=sharing
  */
 export const IMAGES = {
-  dress: "https://drive.google.com/uc?export=view&id=1ybRsWeQryBoA_k0WmW_GNLObIw-Dzpeh",
-  sneakers: "https://drive.google.com/uc?export=view&id=1nYVBcUz7Il177yJd0_uc1ieCT-iUufWH",
-  jacket: "https://drive.google.com/uc?export=view&id=1LcFYaNwRVOdds6FTyo_XSoed1X4AaiQD",
-  bag: "https://drive.google.com/uc?export=view&id=1JXfcMGwoZmfVOIBzLBdNjMwV2GFV5WX_",
-  hat: "https://drive.google.com/uc?export=view&id=1Lj1HJibWpdsaq4ROFj434Mm1FPzeI4up",
-  jeans: "https://drive.google.com/uc?export=view&id=1Qm31Nqz7Kh53ciuStRTMCW0Tm4f4gBBd"
+  dress: "https://lh3.googleusercontent.com/d/1ybRsWeQryBoA_k0WmW_GNLObIw-Dzpeh",
+  sneakers: "https://lh3.googleusercontent.com/d/1nYVBcUz7Il177yJd0_uc1ieCT-iUufWH",
+  jacket: "https://lh3.googleusercontent.com/d/1LcFYaNwRVOdds6FTyo_XSoed1X4AaiQD",
+  bag: "https://lh3.googleusercontent.com/d/1JXfcMGwoZmfVOIBzLBdNjMwV2GFV5WX_",
+  hat: "https://lh3.googleusercontent.com/d/1Lj1HJibWpdsaq4ROFj434Mm1FPzeI4up",
+  jeans: "https://lh3.googleusercontent.com/d/1Qm31Nqz7Kh53ciuStRTMCW0Tm4f4gBBd"
 };
