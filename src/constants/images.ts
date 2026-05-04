8b7e34d88e0eb1e23f4866ede60904027c3ad160
manonmerlet06-web <manonmerlet06@gmail.com>
2026-05-04 14:19:58 +0200

    feat: Update image links
    Updates Google Drive links for static application images. This ensures correct display of fashion items within the OOTD app.

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -3,10 +3,10 @@
  * Format : https://drive.google.com/drive/folders/1Npo3W2tU_AKAaM7f1ijNIwbKi1Nadbj7?usp=sharing
  */
 export const IMAGES = {
-  dress: "https://lh3.googleusercontent.com/d/1ybRsWeQryBoA_k0WmW_GNLObIw-Dzpeh",
-  sneakers: "https://lh3.googleusercontent.com/d/1nYVBcUz7Il177yJd0_uc1ieCT-iUufWH",
-  jacket: "https://lh3.googleusercontent.com/d/1LcFYaNwRVOdds6FTyo_XSoed1X4AaiQD",
-  bag: "https://lh3.googleusercontent.com/d/1JXfcMGwoZmfVOIBzLBdNjMwV2GFV5WX_",
-  hat: "https://lh3.googleusercontent.com/d/1Lj1HJibWpdsaq4ROFj434Mm1FPzeI4up",
-  jeans: "https://lh3.googleusercontent.com/d/1Qm31Nqz7Kh53ciuStRTMCW0Tm4f4gBBd"
+  dress: "https://drive.google.com/file/d/1BT9rn89nzwu1B2gsCvPBfronfIKiPzn6/view?usp=drive_link",
+  sneakers: "https://drive.google.com/file/d/1Jv6PQuJsxTGScIqFysYbYCtxiLYSjIS-/view?usp=drive_link",
+  jacket: "https://drive.google.com/file/d/1SnOKQk6ZZybd4v8c8kt_n7pA8YbfAgFy/view?usp=drive_link",
+  bag: "https://drive.google.com/file/d/1s0tKuPTjnves9dRWlcEpDqsCPjd5sIeV/view?usp=drive_link",
+  hat: "https://drive.google.com/file/d/1uGhbpjgMzVHiW84A4Hbtw6H3_13GQ0SS/view?usp=drive_link",
+  jeans: "https://drive.google.com/file/d/1z8Kx9yZRNH1SFEZAzqxnXINfMn4C2jRi/view?usp=drive_link"
 };
