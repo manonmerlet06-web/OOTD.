4a0618754ed1a50dcb75f0eb0e5f05746a7befc9
manonmerlet06-web <manonmerlet06@gmail.com>
2026-05-04 15:06:33 +0200

    refactor: Update image URLs to use direct Googleusercontent links
    Switches image URLs from Google Drive shared file links to direct Googleusercontent links. This change improves image loading performance and reliability by bypassing the Google Drive viewer.

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -3,10 +3,10 @@
  * Format : https://drive.google.com/file/d/1BT9rn89nzwu1B2gsCvPBfronfIKiPzn6/view?usp=sharing
  */
 export const IMAGES = {
-  dress: "https://drive.google.com/file/d/1BT9rn89nzwu1B2gsCvPBfronfIKiPzn6/view?usp=drive_link",
-  sneakers: "https://drive.google.com/file/d/1Jv6PQuJsxTGScIqFysYbYCtxiLYSjIS-/view?usp=drive_link",
-  jacket: "https://drive.google.com/file/d/1SnOKQk6ZZybd4v8c8kt_n7pA8YbfAgFy/view?usp=drive_link",
-  bag: "https://drive.google.com/file/d/1s0tKuPTjnves9dRWlcEpDqsCPjd5sIeV/view?usp=drive_link",
-  hat: "https://drive.google.com/file/d/1uGhbpjgMzVHiW84A4Hbtw6H3_13GQ0SS/view?usp=drive_link",
-  jeans: "https://drive.google.com/file/d/1z8Kx9yZRNH1SFEZAzqxnXINfMn4C2jRi/view?usp=drive_link"
+  dress: "https://lh3.googleusercontent.com/d/1BT9rn89nzwu1B2gsCvPBfronfIKiPzn6",
+  sneakers: "https://lh3.googleusercontent.com/d/1Jv6PQuJsxTGScIqFysYbYCtxiLYSjIS-",
+  jacket: "https://lh3.googleusercontent.com/d/1SnOKQk6ZZybd4v8c8kt_n7pA8YbfAgFy",
+  bag: "https://lh3.googleusercontent.com/d/1s0tKuPTjnves9dRWlcEpDqsCPjd5sIeV",
+  hat: "https://lh3.googleusercontent.com/d/1uGhbpjgMzVHiW84A4Hbtw6H3_13GQ0SS",
+  jeans: "https://lh3.googleusercontent.com/d/1z8Kx9yZRNH1SFEZAzqxnXINfMn4C2jRi"
 };
